2825bb97a984aa78b1167eaf7f92a6de8144e586
Francesco Nicoletta Puzzillo <francesconicoletta@nomnp.com>
2022-04-09 08:37:47 +0200

    fix calibrate_cpu_cycles loop

--- src/main.c
+++ src/main.c
@@ -17,11 +17,11 @@ void init_thread(void *dummy1, void *dummy2, void *dummy3)
 	ARG_UNUSED(dummy2);
 	ARG_UNUSED(dummy3);
 
-	int p_load = 0;
+	uint32_t p_load = 0;
 
 	printk("init_thread started \n");
 	p_load = calibrate_cpu_cycles();
-	printk("%d", p_load);
+	printk("p_load: %d \n", p_load);
 
 	printk("init_thread abort\n");
 	k_thread_abort(&init_thread_data);
@@ -29,11 +29,22 @@ void init_thread(void *dummy1, void *dummy2, void *dummy3)
 
 void main(void)
 {
-	k_thread_create(&init_thread_data, init_thread_stack_area,
+	k_tid_t init_id;
+	k_thread_runtime_stats_t rt_stats_thread;
+
+	init_id = k_thread_create(&init_thread_data, init_thread_stack_area,
 			K_THREAD_STACK_SIZEOF(init_thread_stack_area),
 			init_thread, NULL, NULL, NULL,
 			INIT_PRIORITY, 0, K_FOREVER);
 	k_thread_name_set(&init_thread_data, "init_thread");
 
 	k_thread_start(&init_thread_data);
+
+	k_thread_runtime_stats_get(init_id, &rt_stats_thread);
+
+	/*
+	 * with CONFIG_SCHED_THREAD_USAGE_ANALYSIS we can use current_cycles,
+	 * peak_cycles, average_cycles
+	 */
+	printk("Cycles: %llu\n", rt_stats_thread.execution_cycles);
 }
--- src/rt_zephyr.c
+++ src/rt_zephyr.c
@@ -5,16 +5,18 @@
 #include <zephyr.h>
 #include <stdio.h>
 #include "rt_zephyr.h"
+#include "rt_zephyr_types.h"
 
 void waste_cpu_cycles(int load_loops)
 {
-	double param, result;
+	double param;
 	double n;
+	double result;
 	unsigned long long i;
 
 	param = 0.95;
 	n = 4;
-	for (i = 0; i < load_loops; i++) {
+	for (i = 0; i < load_loops * 4; i++) {
 		result = ldexp(param, (ldexp(param, ldexp(param, n))));
 		result = ldexp(param, (ldexp(param, ldexp(param, n))));
 		result = ldexp(param, (ldexp(param, ldexp(param, n))));
@@ -25,39 +27,26 @@ void waste_cpu_cycles(int load_loops)
 
 int calibrate_cpu_cycles(void)
 {
-	int nsec_per_loop, avg_per_loop = 0;
-	int max_load_loop = 10000;
-	int cal_trial = 1000;
-	int64_t uptime, uptime_d;
+	int max_load_loop = 1000, cal_trial = 1000;
+	uint32_t start_time, stop_time, nsec_spent, nsec_per_loop, avg_per_loop = 0;
 
 	while (cal_trial) {
 		cal_trial--;
-
-		uptime = k_uptime_get();
+		start_time = k_cycle_get_32();
 		waste_cpu_cycles(max_load_loop);
-		uptime_d = k_uptime_delta(&uptime);
+		stop_time = k_cycle_get_32();
 
-		printf("uptime   : %lld\n", uptime);
-		printf("uptime_d : %lld\n", uptime_d);
-
-		nsec_per_loop = uptime_d / max_load_loop;
+		/* TODO: check overflow */
+		nsec_spent = k_cyc_to_ns_floor32(stop_time - start_time);
+		nsec_per_loop = nsec_spent / max_load_loop;
 		avg_per_loop = (avg_per_loop + nsec_per_loop) >> 1;
 
-		printf("nsec: %d\n", nsec_per_loop);
-		printf("avg : %d\n", avg_per_loop);
-
-		/* collect a critical mass of samples.*/
 		if ((abs(nsec_per_loop - avg_per_loop) * 50) < avg_per_loop)
 			return avg_per_loop;
 
-		/*
-		* use several loop duration in order to be sure to not
-		* fall into a specific platform loop duration
-		* (like the cpufreq period)
-		*/
-		/* randomize the number of loops and recheck 1000 times */
-		max_load_loop += 33333;
-		max_load_loop %= 1000000;
+		max_load_loop += 3333;
+		max_load_loop %= 100000;
 	}
+
 	return 0;
 }
